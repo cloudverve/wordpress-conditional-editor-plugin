de1cfc077af63c78bcf084f54ac4f7d55326595a
Daniel Hendricks <mail@danhendricks.com>
2018-07-08 22:06:22 -0500

    Added code to disable Gutenberg completely

--- app/Core.php
+++ app/Core.php
@@ -14,6 +14,10 @@ class Core extends Plugin {
 
     $this->disable_gutenberg = $this->get_carbon_plugin_option( 'disable_gutenberg' );
 
+    // Disable Gutenberg editor completely
+    if( $this->disable_gutenberg )
+      add_filter( 'gutenberg_can_edit_post_type', '__return_false' );
+
     // Disable "Try Gutenberg" notice/nag
     if( $this->disable_gutenberg || $this->get_carbon_plugin_option( 'disable_gutenberg_nag' ) )
       remove_filter( 'try_gutenberg_panel', 'wp_try_gutenberg_panel' );
